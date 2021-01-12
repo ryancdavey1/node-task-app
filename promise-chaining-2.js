114f03ce0cbc9cc26a949bac723872eb83246c4d
ryancdavey1 <ryancdavey1@gmail.com>
2021-01-11 18:42:01 -0700

    change express routes to be async/await

--- promise-chaining-2.js
+++ promise-chaining-2.js
@@ -1,12 +1,24 @@
 require('./src/db/mongoose.js');
 const Task = require('./src/models/task');
 
-Task.findByIdAndDelete('5ff6a73f78386ff8165bea39')
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  }).then((result) => {
-    console.log(result);
-  }).catch((e) => {
-    console.log(e);
-  });
+// Task.findByIdAndDelete('5ff6a73f78386ff8165bea39')
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   }).then((result) => {
+//     console.log(result);
+//   }).catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count
+}
+
+deleteTaskAndCount('5ffa9ec9cb349e99077d8100').then((count) => {
+  console.log(count);
+}).catch((e) => {
+  console.log(e);
+});
